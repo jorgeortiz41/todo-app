278eb9875b67db3fc2a234517213b1235bb36e74
Jorge A Ortiz <jorge.ortiz41@upr.edu>
2022-12-18 23:54:22 -0400

    added add functionality, working on delete

--- components/TodoList.js
+++ components/TodoList.js
@@ -10,6 +10,8 @@ import IconButton from '@mui/material/IconButton';
 import CommentIcon from '@mui/icons-material/Comment';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { ButtonGroup } from '@mui/material';
+import { Divider } from '@mui/material';
 
 
 export default function TodoList() {
@@ -28,6 +30,51 @@ export default function TodoList() {
         setLoading(false)
       })
   }, [])
+
+  //handle task editing by id!!!!MISSING!!!!
+  // const handleEdit = (id) => {
+  //   const editTask = async () => {
+  //       const response = await fetch('http://localhost:5000/api/edittask/' + id, {
+  //           method: 'PUT',
+  //           headers: {
+  //               'Content-Type': 'application/json',
+  //           },
+  //           body: JSON.stringify({
+  //               name: 'edited task',
+  //           }),
+  //       });
+  //       const body = await response.json();
+  //       if (response.status !== 200) {
+  //           throw Error(body.message)
+  //       }
+  //       return body;
+  //   };
+  //   editTask()
+  //       .then(res => console.log(res))
+  //       .catch(err => console.log(err));
+  //   window.location.reload(false);
+  // }
+
+  //handle task deletion
+  const handleDelete = (id) => {
+    const deleteTask = async () => {
+        const response = await fetch('http://localhost:5000/api/erasetask/' + id.toString(), {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const body = await response.json();
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body;
+    };
+    deleteTask()
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    window.location.reload(false);
+}
 
   //handle checkbox behavior
   const handleToggle = (value) => () => {
@@ -50,21 +97,26 @@ export default function TodoList() {
 
 
   return (
-    <List sx={{ width: '100%', maxWidth: 600, bgcolor: 'background.paper' }}>
+    <List sx={{ width: '100%', maxWidth: "80%", bgcolor: 'background.paper' }}>
     {data.map((task) => {
       const labelId = `checkbox-list-label-${task.name}`;
 
       return (
+        <>
         <ListItem
           key={task.id}
           secondaryAction={
-            <IconButton edge="end" aria-label="comments">
-              <CommentIcon />
-              <EditIcon />
-              <DeleteIcon />
-            </IconButton>
+            <ButtonGroup variant="outlined" aria-label="text button group" style={{ width: 100, justifyContent: "space-evenly" }}>
+              <IconButton edge='end' aria-label='edit'>
+                <EditIcon />
+              </IconButton>
+              <IconButton edge='end' aria-label='delete'>
+                <DeleteIcon />
+              </IconButton>
+            </ButtonGroup>
           }
           disablePadding
+
         >
           <ListItemButton role={undefined} onClick={handleToggle(task)} dense>
             <ListItemIcon>
@@ -79,6 +131,8 @@ export default function TodoList() {
             <ListItemText id={labelId} primary={task.name} />
           </ListItemButton>
         </ListItem>
+        <Divider />
+        </>
       );
     })}
   </List>
