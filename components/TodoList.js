3fcbdd4cbbe59455b7352851dc0b64b998f95499
Jorge A Ortiz <jorge.ortiz41@upr.edu>
2022-12-24 06:25:30 -0400

    got edit to work partially, missing status

--- components/TodoList.js
+++ components/TodoList.js
@@ -20,7 +20,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import MenuItem from '@mui/material/MenuItem';
 import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+
 
 
 export default function TodoList() { 
@@ -63,7 +63,10 @@ export default function TodoList() {
   const [isLoading, setLoading] = useState(false)
   const [checked, setChecked] = React.useState([0]);
   const [open, setOpen] = React.useState([0]);
-  const [task, setTask] = useState(null)
+  const [name, setName] = useState('')
+  const [stat, setStat] = useState('')
+  const [notes, setNotes] = useState('')
+  const [category, setCategory] = useState('')
 
 
   //fetch tasks from api
@@ -78,41 +81,33 @@ export default function TodoList() {
       })
   }, [])
 
-  //function to get tasks from database
-  const getTasks = async () => {
-    const response = await fetch('http://localhost:5000/api/tasks');
-    const body = await response.json();
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
+  ////////////////////////////ON-CHANGE EFFECTS/////////////////////////////////////////////////
 
-    return body;
+  const handleNameChange = (event) => {
+    //use setTask to update the task object
+    setName(event.target.value)
   };
 
-  //handle task editing by id!!!!MISSING!!!!
-  // const handleEdit = (id) => {
-  //   const editTask = async () => {
-  //       const response = await fetch('http://localhost:5000/api/edittask/' + id, {
-  //           method: 'PUT',
-  //           headers: {
-  //               'Content-Type': 'application/json',
-  //           },
-  //           body: JSON.stringify({
-  //               name: 'edited task',
-  //           }),
-  //       });
-  //       const body = await response.json();
-  //       if (response.status !== 200) {
-  //           throw Error(body.message)
-  //       }
-  //       return body;
-  //   };
-  //   editTask()
-  //       .then(res => console.log(res))
-  //       .catch(err => console.log(err));
-  //   window.location.reload(false);
-  // }
+  const handleStatusChange = (event) => {
+    // event.target.value
+    if (event.target.value === "to-do") {
+      setStat("to-do");
+    } else if (event.target.value === "in-progress") {
+      setStat("in-progress");
+    } else {
+      setStat("done");
+    }
+  };
 
+  const handleNotesChange = (event) => {
+    setNotes(event.target.value)
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value)
+  };
+
+  //////////////////////////CRUD/////////////////////////////////////////////////
 
   //handle task deletion by id
   const handleDelete = (id) => {
@@ -136,6 +131,30 @@ export default function TodoList() {
     window.location.reload(false);
   }
 
+  //async function to take dialog field values and update task in database
+  const updateTask = async (value) => {
+    const newName = name === '' ? value.name : name;
+    const newNotes = notes === '' ? value.notes : notes;
+    const newCategory = category === '' ? value.cat : category;
+    const response = await fetch('http://localhost:5000/api/updatetask/' + value._id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: newName,
+            notes: newNotes,
+            cat: newCategory,
+        }),
+    });
+    const body = await response.json();
+    if (response.status !== 200) {
+        throw Error(body.message)
+    }
+
+    return body;
+  };
+
   //handle task isDone property update by id
   const updateStatusTask = async (value) => {
     const isDone = !value.isDone;
@@ -153,7 +172,7 @@ export default function TodoList() {
         throw Error(body.message)
     }
     return body;
-  }
+  };
 
   const updateImportanceTask = async (value) => {
     const isImportant = !value.isImportant;
@@ -171,7 +190,9 @@ export default function TodoList() {
         throw Error(body.message)
     }
     return body;
-  }
+  };
+
+  ////////////////////////////TOGGLES/////////////////////////////////////
 
   //handle checkbox behavior
   const handleToggle = (value) => () => {
@@ -198,7 +219,7 @@ export default function TodoList() {
     window.location.reload(false);
   };
 
-  //handle task dialong toggle
+  //handle task dialog toggle
   const handleDialogToggle = (value) => () => {
     const currentIndex = open.indexOf(value);
     const newOpen = [...open];
@@ -212,6 +233,7 @@ export default function TodoList() {
     setOpen(newOpen);
   };
 
+  //handle task importance toggle
   const handleImportantToggle = (value) => () => {
     //call updateImportanceTask function
     updateImportanceTask(value)
@@ -220,9 +242,19 @@ export default function TodoList() {
         console.log("task importance has been updated");
 
     window.location.reload(false);
-  }
+  };
 
-
+  //handle task dialog submit
+  const handleClose = (value) => {
+    //update task in database
+    updateTask(value)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+        console.log("task has been edited");
+    
+    handleDialogToggle(value);
+    window.location.reload(false);
+  };
 
   //displayed when loading or no data
   if (isLoading) return <p>Loading...</p>
@@ -236,7 +268,7 @@ export default function TodoList() {
     <ListSubheader>To-do</ListSubheader>
     <Divider variant="middle" />
     {/* Display tasks checked if isDone is true and unchecked if isDone is false */}
-    {data.map((task) => {
+    {data.map((task, i) => {
       const labelId = `checkbox-list-label-${task._id}`;
 
       if (!task.isDone) {
@@ -266,59 +298,86 @@ export default function TodoList() {
               <ListItemText id={labelId} primary={task.name} />
             </ListItemButton>
           </ListItem>
-          <Dialog
-          fullWidth={true}
-          open={open.indexOf(task)!== -1} 
-          onClose={handleDialogToggle(task)} 
-          key={task._id}
-          >
-          <DialogTitle>
-          <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label={task.name}
-              type="text"
-              fullWidth
-              variant="outlined"
-            />
-          </DialogTitle>
-          <DialogContent>
-            <TextField
-              margin="dense"
-              id="outlined-select-status"
-              select
-              label="Status"
-              defaultValue={task.inProg ? 'in-progress': task.isDone ? 'done': 'to-do' } 
-              helperText="Change status"
+            <Dialog
+            fullWidth={true}
+            open={open.indexOf(task)!== -1} 
+            onClose={handleDialogToggle(task)} 
+            key={task._id}
             >
-              {status.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
+            <DialogTitle>
             <TextField
-              margin="dense"
-              id="outlined-select-category"
-              select
-              label="Category"
-              defaultValue={task.cat} 
-              helperText="Change category"
-              sx={{ml: 2}}
-            >
-              {categories.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>       
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleDialogToggle(task)}>Cancel</Button>
-            <Button onClick={handleDialogToggle(task)}>Confirm</Button>
-          </DialogActions>
-        </Dialog>
+                autoFocus
+                margin="dense"
+                id="name"
+                label={task.name}
+                value={name}
+                defaultValue={task.name}
+                onChange= {handleNameChange}
+                type="text"
+                fullWidth
+                variant="outlined"
+                helperText="Change task name"
+  
+              />
+            </DialogTitle>
+            <DialogContent>
+              <TextField
+                margin="dense"
+                id="outlined-select-status"
+                select
+                label='Status'
+                defaultValue='to-do' 
+                helperText="Change status"
+                value={stat}
+                onChange={handleStatusChange}
+                disabled
+              >
+                {status.map((stat) => (
+                  <MenuItem key={stat.value} value={stat.value}>
+                    {stat.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                margin="dense"
+                id="outlined-select-category"
+                select
+                placeholder={task.cat}
+                label={task.cat}
+                defaultValue={task.cat} 
+                value={category}
+                onChange={handleCategoryChange}
+                helperText="Change category"
+                sx={{ml: 2}}
+              >
+                {categories.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label={task.notes}
+                value={notes}
+                defaultValue={task.notes}
+                onChange= {handleNotesChange}
+                type="text"
+                fullWidth
+                variant="outlined"
+                multiline
+                helperText="Notes"
+                maxRows={6}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleDialogToggle(task)}>Cancel</Button>
+              <Button onClick={() => handleClose(task)}>Confirm</Button>
+            </DialogActions>
+            </Dialog>
+    
         </>
         )
       }
@@ -340,6 +399,7 @@ export default function TodoList() {
           <>
           <ListItem key={task._id} secondaryAction={
             <ButtonGroup>
+              <Rating name="customized-1" defaultValue={task.isImportant ? 1: 0} max={1} sx={{p:1, pr:0}} size='large' onClick={handleImportantToggle(task)}/>
               <IconButton edge="end" aria-label="edit" onClick={handleDialogToggle(task)}>
                 <EditIcon />
               </IconButton>
@@ -384,8 +444,11 @@ export default function TodoList() {
               id="outlined-select-currency"
               select
               label="Status"
-              defaultValue={task.inProg ? 'in-progress': task.isDone ? 'done': 'to-do' } 
+              defaultValue='to-do'
               helperText="Change status"
+              value={stat}
+              onChange={handleStatusChange}
+
             >
               {status.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -393,10 +456,28 @@ export default function TodoList() {
                 </MenuItem>
               ))}
             </TextField>
+            <TextField
+                margin="dense"
+                id="outlined-select-category"
+                select
+                label={task.cat}
+                placeholder={task.cat}
+                defaultValue={task.cat} 
+                value={category}
+                onChange={handleCategoryChange}
+                helperText="Change category"
+                sx={{ml: 2}}
+              >
+                {categories.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
           </DialogContent>
           <DialogActions>
             <Button onClick={handleDialogToggle(task)}>Cancel</Button>
-            <Button onClick={handleDialogToggle(task)}>Confirm</Button>
+            <Button onClick={handleClose(value)}>Confirm</Button>
           </DialogActions>
         </Dialog>
           </>
